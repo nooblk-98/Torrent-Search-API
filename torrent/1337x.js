686541252acb404b93bf9d023d29edbd34f6be05
NoobLk <liyanagelsofficial@gmail.com>
2026-05-28 10:44:26 +0530

    Fix 1337x and bitsearch scrapers for updated HTML structures

--- torrent/1337x.js
+++ torrent/1337x.js
@@ -1,15 +1,17 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
 
-
-
 async function torrent1337x(query = '', page = '1') {
-
     const allTorrent = [];
     let html;
     const url = 'https://1337xx.to/search/' + query + '/' + page + '/';
     try {
-        html = await axios.get(url);
+        html = await axios.get(url, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+                'Referer': 'https://1337xx.to/'
+            }
+        });
     } catch {
         return null;
     }
@@ -19,30 +21,33 @@ async function torrent1337x(query = '', page = '1') {
     const links = $('td.name').map((_, element) => {
         var link = 'https://1337xx.to' + $(element).find('a').next().attr('href');
         return link;
-
     }).get();
 
-
     await Promise.all(links.map(async (element) => {
-
         const data = {};
         const labels = ['Category', 'Type', 'Language', 'Size', 'UploadedBy', 'Downloads', 'LastChecked', 'DateUploaded', 'Seeders', 'Leechers'];
         let html;
         try {
-            html = await axios.get(element);
+            html = await axios.get(element, {
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+                    'Referer': 'https://1337xx.to/'
+                }
+            });
         } catch {
             return null;
         }
         const $ = cheerio.load(html.data);
         data.Name = $('.box-info-heading h1').text().trim();
-        data.Magnet = $('.clearfix ul li a').attr('href') || "";
-        const poster = $('div.torrent-image img').attr('src');
+        data.Magnet = $('a[href^="magnet:"]').attr('href') || "";
+
+        const poster = $('.torrent-image img').attr('src');
 
         if (typeof poster !== 'undefined') {
-            if (poster.startsWith('http')) {
+            if (poster && poster.startsWith('http')) {
                 data.Poster = poster;
             }
-            else {
+            else if (poster) {
                 data.Poster = 'https:' + poster;
             }
         } else {
